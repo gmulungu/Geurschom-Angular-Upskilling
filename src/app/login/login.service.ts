9d1e4733de15f7cd7c719d8327833b9cee700a12
Geurschomm <geurschom.mulungu@redpandasoftware.co.za>
2024-12-04 11:04:47 +0200

    First Demo for the Angular Project

---
+++ src/app/login/login.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  isLoggedIn: boolean = false;
+
+  isAdmin: boolean = false;
+
+  constructor() {}
+
+  login(email: string, password: string) {
+    if (email === 'Geurschom@gmail.com' && password === 'Genesis') {
+      this.isLoggedIn = true;
+      this.isAdmin = true;
+    }
+    if (email === 'user@gmail.com' && password === 'user') {
+       this.isLoggedIn = true;
+       this.isAdmin = false;
+    }
+      return this.isLoggedIn;
+  }
+}
